23937d017148a07e3e261bd4263ffb9de7dd319f
SHIYU ZHANG <18429076@life.hkbu.edu.hk>
2018-10-21 20:08:15 +0800

    new_updated

--- assignment0/keywords.py
+++ assignment0/keywords.py
@@ -1,45 +1,55 @@
-import os # I found this module on the Internet, and I downloaded all the text files to my local path
-myFiles = ['trade-wars-news1.txt', 'trade-wars-news2.txt', 'trade-wars-news3.txt', 'trade-wars-news4.txt', 'trade-wars-news5.txt']
-articles = []
-for filename in myFiles:
-    os.path.join('/Users/cathychang/desktop/friday/assignment0', filename)
-    article = os.path.join('/Users/cathychang/desktop/friday/assignment0', filename)
-    articles.append(article)
-#print(articles)
-my_data = []
-for i in articles:
-    data = open(i).read() #assemble the content of all 5 files and append them in a list
-    my_data.append(data)
-#print(my_data)
-split_words = []
-for elements in my_data:
-    splited = str.split(elements) 
-    split_words.append(splited)
-#print(split_words)
+import requests
+
+#Read all the .txt files in folder assignment0/
+content = []
+for i in range(1,6):
+    url="https://raw.githubusercontent.com/hupili/python-for-data-and-media-communication-gitbook/master/assets/assignment0/trade-wars-news{}.txt".format(i)
+    s=requests.get(url).text
+    content.append(s)
+    
+import string
+without_punc = []  #remove all punctuations in the texts
+for elements in content:
+    table = str.maketrans({key: None for key in string.punctuation})
+    new_s = elements.translate(table) 
+    without_punc.append(new_s)
 
 import nltk  # To clean the data and remove the stop words, I googled the solution and found NLTK(Natural Language Toolkit) in python 
 nltk.download('punkt')
 from nltk.corpus import stopwords 
 en_stops = set(stopwords.words('english'))
 
-filtered_data = [] #put the filtered words in a list
-for elements in split_words:
-    for word in elements: 
-        lower = str.lower(word)
-        if lower not in en_stops:
-            #print(word)
-            filtered_data.append(lower)
+filtered_data = []
+lower_data = [] #lower the words in the texts
+for w in without_punc: 
+    lower = str.lower(w)
+    lower_data.append(lower)
+    
+split_words = []
+for elements in lower_data: # split the string
+    splited = str.split(elements) 
+    split_words.append(splited)
 
-import csv
-dict = {}
+for elements in split_words: # remove stop words
+    for w in elements:
+        if w not in en_stops:
+            filtered_data.append(w)
+
+without_stopwords = []
+stop_words = ['also', 'would', 'us', 'said'] #enrich this stop_words list to make the output more meaningful
 for w in filtered_data:
-    dict[w] = filtered_data.count(w)#For every word extracted above, count how many times each one appears.
+    if w not in stop_words:
+        without_stopwords.append(w)
+
+dict = {}
+for word in without_stopwords:
+    dict[word] = without_stopwords.count(word)#For every word extracted above, count how many times each one appears.
     #print(dict)
     frequency = sorted(dict.items(), key=lambda d: d[1], reverse = True) # arrange the words frequency in ascending order
     #print(frequency)
-    with open('assignment0.csv','w') as f:
-        writer = csv.writer(f)
-        header = ['keyword','frequency']
-        writer.writerow(header)
-        writer.writerows(frequency[:15])           
-   
+import csv
+with open('assignment0.csv','w') as f: #Output the full keyword frequency list into a CSV file
+    writer = csv.writer(f)
+    header = ['keyword','frequency']
+    writer.writerow(header)
+    writer.writerows(frequency[:15])  
